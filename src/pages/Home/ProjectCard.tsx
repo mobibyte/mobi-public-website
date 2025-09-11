c28896156db40729f817e11d7117dab5f2996247
David Cai <43160344+davidxcai@users.noreply.github.com>
2025-09-11 13:42:54 -0500

    Added pagination to projects page

--- src/pages/Home/ProjectCard.tsx
+++ src/pages/Home/ProjectCard.tsx
@@ -1,4 +1,11 @@
-import { Box, Image, HStack, VStack, Text } from "@chakra-ui/react";
+import {
+  AspectRatio,
+  Box,
+  Image,
+  HStack,
+  VStack,
+  Text,
+} from "@chakra-ui/react";
 import type { Project } from "@/types";
 
 // TODO:
@@ -6,30 +13,26 @@ import type { Project } from "@/types";
 // when user clicks project, navigate to website
 
 export function ProjectCard({ project }: { project: Project }) {
-    return (
-        <Box overflow="hidden" _hover={{ boxShadow: "lg" }}>
-            <Image
-                src={project.image}
-                alt={project.title}
-                objectFit="cover"
-                height={260}
-                aspectRatio={16 / 10}
-            />
-            <HStack py={4} gap={4}>
-                <Image
-                    rounded={"full"}
-                    aspectRatio={1}
-                    h={12}
-                    src={project.userImg}
-                    alt={project.userImg}
-                />
-                <VStack align={"start"} gap={0}>
-                    <Text fontSize="md" fontWeight={700}>
-                        {project.title}
-                    </Text>
-                    <Text>{project.user}</Text>
-                </VStack>
-            </HStack>
-        </Box>
-    );
+  return (
+    <Box overflow="hidden" _hover={{ boxShadow: "lg" }} flex={"grow"}>
+      <AspectRatio ratio={16 / 10}>
+        <Image src={project.image} alt={project.title} objectFit="cover" />
+      </AspectRatio>
+      <HStack py={4} gap={4}>
+        <Image
+          rounded={"full"}
+          aspectRatio={1}
+          h={12}
+          src={project.userImg}
+          alt={project.userImg}
+        />
+        <VStack align={"start"} gap={0}>
+          <Text fontSize="md" fontWeight={700}>
+            {project.title}
+          </Text>
+          <Text>{project.user}</Text>
+        </VStack>
+      </HStack>
+    </Box>
+  );
 }
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,90 @@
+import { projects } from "@/data/projects";
+import {
+  Stack,
+  IconButton,
+  ButtonGroup,
+  Heading,
+  Pagination,
+  Grid,
+} from "@chakra-ui/react";
+import { Reveal } from "@/components/ui/Reveal";
+import { ProjectCard } from "./Home/ProjectCard";
+import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
+import { useState } from "react";
+
 export function Projects() {
-    return (
-        <div className="min-h-dvh flex justify-center items-center">
-            Projects Page
-        </div>
-    );
+  const pageSize = 9; // 9 Projects per page
+  const [page, setPage] = useState(1);
+
+  const startRange = (page - 1) * pageSize;
+  const endRange = startRange + pageSize;
+
+  const visibleProjects = projects.slice(startRange, endRange);
+  return (
+    <>
+      <Stack
+        gap={12}
+        width={"100%"}
+        px={{ base: 4, md: 32 }}
+        py={32}
+        position={"relative"}
+        minHeight={"dvh"}
+      >
+        <Heading
+          fontSize={48}
+          className="space-grotesk-500"
+          textAlign={"center"}
+          lineHeight={1}
+          zIndex={2}
+        >
+          Community Projects
+        </Heading>
+        <Grid
+          templateColumns={{
+            base: "1fr",
+            sm: "repeat(2, 1fr)",
+            md: "repeat(3, 1fr)",
+          }}
+          gap="6"
+          flex={"grow"}
+        >
+          {visibleProjects.map((project, index) => (
+            <Reveal delay={150}>
+              <ProjectCard key={project.title + index} project={project} />
+            </Reveal>
+          ))}
+        </Grid>
+      </Stack>
+      <Pagination.Root
+        count={projects.length}
+        pageSize={pageSize}
+        page={page}
+        onPageChange={(e) => setPage(e.page)}
+        mx={"auto"}
+        alignSelf={"end"}
+      >
+        <ButtonGroup variant="ghost" size="sm">
+          <Pagination.PrevTrigger asChild>
+            <IconButton>
+              <IconChevronLeft />
+            </IconButton>
+          </Pagination.PrevTrigger>
+
+          <Pagination.Items
+            render={(page) => (
+              <IconButton variant={{ base: "ghost", _selected: "outline" }}>
+                {page.value}
+              </IconButton>
+            )}
+          />
+
+          <Pagination.NextTrigger asChild>
+            <IconButton>
+              <IconChevronRight />
+            </IconButton>
+          </Pagination.NextTrigger>
+        </ButtonGroup>
+      </Pagination.Root>
+    </>
+  );
 }
