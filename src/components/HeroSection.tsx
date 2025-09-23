1114bd39117a32ac279619a5ef93c9ff3ebb52f2
David Cai <43160344+davidxcai@users.noreply.github.com>
2025-09-23 14:39:18 -0500

    Fixed merge conflict

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,15 +1,18 @@
 import {
-    Center,
-    Stack,
-    VStack,
-    Button,
-    Heading,
-    Text,
-    useBreakpointValue,
-    HStack,
+  Center,
+  Stack,
+  VStack,
+  Button,
+  Heading,
+  Text,
+  useBreakpointValue,
+  HStack,
 } from "@chakra-ui/react";
 import { DynamicMomo } from "@/assets/DynamicMomo";
-import { IconBrandDiscordFilled, IconBrandInstagram } from "@tabler/icons-react";
+import {
+  IconBrandDiscordFilled,
+  IconBrandInstagram,
+} from "@tabler/icons-react";
 import { Reveal } from "./ui/Reveal";
 import { StarsBackground } from "@/assets/Stars";
 
@@ -41,72 +44,61 @@ export function HeroSection() {
           className="space-grotesk-500"
           textShadow={textShadow}
         >
-            <StarsBackground />
-            <VStack
-                align={isMobile ? "center" : "left"}
-                flex={{ base: "0 0 auto", md: 1 }}
+          MOBI
+        </Heading>
+        <Heading
+          fontSize={isMobile ? 90 : 130}
+          lineHeight={1}
+          className="space-grotesk-500"
+          color={"#0084FF"}
+          textShadow={textShadow}
+        >
+          BYTE
+        </Heading>
+        <Text
+          fontSize={24}
+          fontWeight={700}
+          textAlign={isMobile ? "center" : "left"}
+        >
+          The Web & App Development club open to everyone from beginner to
+          wizard. 🚀
+        </Text>
+        <HStack>
+          <a href="https://discord.gg/xsY7HxSdvp" target="_blank">
+            <Button
+              bg="#7289da"
+              alignSelf={isMobile ? "center" : "flex-start"}
+              marginTop={4}
+              shadow={"lg"}
+              color={"white"}
+              fontWeight={700}
             >
-                <Heading
-                    fontSize={isMobile ? 90 : 130}
-                    lineHeight={0.6}
-                    className="space-grotesk-500"
-                    textShadow={textShadow}
-                >
-                    MOBI
-                </Heading>
-                <Heading
-                    fontSize={isMobile ? 90 : 130}
-                    lineHeight={1}
-                    className="space-grotesk-500"
-                    color={"#0084FF"}
-                    textShadow={textShadow}
-                >
-                    BYTE
-                </Heading>
-                <Text
-                    fontSize={24}
-                    fontWeight={700}
-                    textAlign={isMobile ? "center" : "left"}
-                >
-                    The Web & App Development club open to everyone from
-                    beginner to wizard. 🚀
-                </Text>
-                <HStack>
-                    <a href="https://discord.gg/xsY7HxSdvp" target="_blank">
-                    <Button
-                        bg="#7289da"
-                        alignSelf={isMobile ? "center" : "flex-start"}
-                        marginTop={4}
-                        shadow={"lg"}
-                        color={"white"}
-                        fontWeight={700}
-                    >
-                        <IconBrandDiscordFilled />
-                        Discord
-                    </Button>
-                    </a>
-                    <a href="https://www.instagram.com/codewithmobi/" target="_blank">
-                        <Button
-                            bg="#C13584"
-                            alignSelf={isMobile ? "center" : "flex-start"}
-                            marginTop={4}
-                            shadow={"lg"}
-                            color={"white"}
-                            fontWeight={700}
-                        >
-                        <IconBrandInstagram />
-                        Instagram
-                    </Button>
-                    </a>
-                </HStack>
-            </VStack>
-            {!isMobile && (
-                <Center flex={1}>
-                    <Reveal delay={300}>
-                        <DynamicMomo />
-                    </Reveal>
-                </Center>
-            )}
-        </Stack>
-    );
+              <IconBrandDiscordFilled />
+              Discord
+            </Button>
+          </a>
+          <a href="https://www.instagram.com/codewithmobi/" target="_blank">
+            <Button
+              bg="#C13584"
+              alignSelf={isMobile ? "center" : "flex-start"}
+              marginTop={4}
+              shadow={"lg"}
+              color={"white"}
+              fontWeight={700}
+            >
+              <IconBrandInstagram />
+              Instagram
+            </Button>
+          </a>
+        </HStack>
+      </VStack>
+      {!isMobile && (
+        <Center flex={1}>
+          <Reveal delay={300}>
+            <DynamicMomo />
+          </Reveal>
+        </Center>
+      )}
+    </Stack>
+  );
 }
