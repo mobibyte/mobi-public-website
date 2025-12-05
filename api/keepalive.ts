cb594d2237bac65b2408119b26619b657827cc9c
David Cai <43160344+davidxcai@users.noreply.github.com>
2025-12-05 11:22:28 -0600

    Added comments and updated ping url

--- api/keepalive.ts
+++ api/keepalive.ts
@@ -1,5 +1,10 @@
 import { supabase } from "../src/hooks/supabaseClient";
 
+// Hi, don't delete this!
+// We need this serverless function for our cron job
+// It'll ping the website every 12 hours to keep it alive
+// and that prevents supabase from pausing the database
+
 export default async function handler(req: any, res: any) {
     try {
         await supabase.from("projects").select("id", { limit: 1 });
