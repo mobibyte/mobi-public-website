daa061b3610a552965aa73f05b491ca97d86e524
David Cai <43160344+davidxcai@users.noreply.github.com>
2026-01-05 14:01:00 -0600

    commented out unused functions

--- src/helpers/colors.ts
+++ src/helpers/colors.ts
@@ -31,33 +31,33 @@ export function useAverageImageColor(
     return { bgColor, textColor };
 }
 
-type HSL = [number, number, number]; // [h, s, l]
+// type HSL = [number, number, number]; // [h, s, l]
 
-export function generateHslVariants([h, s, l]: HSL) {
-    const clamp = (v: number, min: number, max: number) =>
-        Math.min(Math.max(v, min), max);
+// export function generateHslVariants([h, s, l]: HSL) {
+//     const clamp = (v: number, min: number, max: number) =>
+//         Math.min(Math.max(v, min), max);
 
-    const light = {
-        h,
-        s: clamp(s * 0.9, 40, 80),
-        l: 92,
-    };
+//     const light = {
+//         h,
+//         s: clamp(s * 0.9, 40, 80),
+//         l: 92,
+//     };
 
-    const medium = {
-        h,
-        s: clamp(s * 1.0, 45, 85),
-        l: 70,
-    };
+//     const medium = {
+//         h,
+//         s: clamp(s * 1.0, 45, 85),
+//         l: 70,
+//     };
 
-    const dark = {
-        h,
-        s: clamp(s * 0.6, 20, 60),
-        l: 18,
-    };
+//     const dark = {
+//         h,
+//         s: clamp(s * 0.6, 20, 60),
+//         l: 18,
+//     };
 
-    return {
-        light: `hsl(${light.h}, ${light.s}%, ${light.l}%)`,
-        medium: `hsl(${medium.h}, ${medium.s}%, ${medium.l}%)`,
-        dark: `hsl(${dark.h}, ${dark.s}%, ${dark.l}%)`,
-    };
-}
+//     return {
+//         light: `hsl(${light.h}, ${light.s}%, ${light.l}%)`,
+//         medium: `hsl(${medium.h}, ${medium.s}%, ${medium.l}%)`,
+//         dark: `hsl(${dark.h}, ${dark.s}%, ${dark.l}%)`,
+//     };
+// }
